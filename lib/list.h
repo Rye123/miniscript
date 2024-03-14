f224a491e04b9ed1007f973a1f9c68d541c2ad8c
Ryan <ryantoh99@gmail.com>
2024-03-14 15:00:18 +0800

    feat (wip): list interface

---
+++ lib/list.h
@@ -0,0 +1,43 @@
+#ifndef _LIST_H_
+#define _LIST_H_
+#include <stdio.h>
+
+typedef struct _node {
+    void *data;
+    struct _node *prev;
+    struct _node *next;
+} ListNode;
+
+typedef struct {
+    size_t elemSz;
+    size_t size;
+    ListNode *head;
+    ListNode *tail;
+} List;
+
+List* list_create(size_t elemSz);
+
+/*
+Adds an item to the list.
+- `data` should be a pointer to the item of size `List.elemSz`. This function will make a COPY of the data and create a new `ListNode` that points to the copied data.
+*/
+void list_add_item(List *ls, const void *data);
+
+/*
+Returns an array of pointers to the values of the list. This takes a PRE-ALLOCATED array and overwrites the values inside with the array of pointers.
+
+Remember to typecast and then dereference to pointers. 
+For instance, if we have a list of integers `List *ls`:
+```c
+int **arr = malloc(ls->size * sizeof(int *));
+arr = (int **) list_to_arr(ls, (void **) arr);
+for (int i = 0; i < ls->size; i++) {
+    printf("%d\n", *(arr[i]));
+}
+```
+*/
+void** list_to_arr(List *ls, void **arr);
+
+void list_free(List *ls);
+
+#endif
---
+++ lib/list_test.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ERR_BUF_SZ 255
+#include "list.h"
+
+void test_assert(int cond_result, const char* resultPass, const char* resultFail)
+{
+    if (cond_result)
+        printf("\033[92mPASS: %s\033[0m\n", resultPass);
+    else {
+        printf("\033[91mFAIL: %s\033[0m\n", resultFail);
+        exit(1);
+    }
+}
+
+int main()
+{
+    char errMsg[ERR_BUF_SZ];
+    // Test empty list
+    List *test_ls_0 = list_create(sizeof(int));
+    int **res_ls_0 = malloc(sizeof(int *) * 0);
+    res_ls_0 = (int**) list_to_arr(test_ls_0, (void **) res_ls_0);
+    test_assert(res_ls_0[0] == NULL, "res_ls_0 is empty", "res_ls_0 is not empty");
+    list_free(test_ls_0);
+    free(res_ls_0);
+
+    // Test add_item
+    int exp_arr_1[5] = {1, 2, 3, 4, 5};
+    List *test_ls_1 = list_create(sizeof(int));
+    for (int i = 0; i < 5; i++)
+        list_add_item(test_ls_1,  exp_arr_1 + i);
+    int **res_ls_1 = malloc(test_ls_1->size * sizeof(int *));
+    res_ls_1 = (int**) list_to_arr(test_ls_1, (void **) res_ls_1);
+    int matched_1 = 1;
+    for (int i = 0; i < 5; i++) {
+        if (*res_ls_1[i] != exp_arr_1[i]) {
+            sprintf(errMsg, "res_ls_1[%d] != test_ls_1[%d], expected %d, got %d", i, i, *res_ls_1[i], exp_arr_1[i]);
+            matched_1 = 0;
+            break;
+        }
+    }
+    test_assert(matched_1, "test_ls_1 matches exp_arr_1", errMsg);
+    list_free(test_ls_1);
+    free(res_ls_1);
+
+    return 0;
+}
